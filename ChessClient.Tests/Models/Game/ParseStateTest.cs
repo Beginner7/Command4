9faee4e92936b48553cac928184650e9b6b6fd96
Iwillbeagod <ravenous944@gmail.com>
2013-11-13 01:20:34 +0400

    Added game state's parsing

---
+++ ChessClient.Tests/Models/Game/ParseStateTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Security.Principal;
+using System.Web;
+using System.Web.Routing;
+using System.Web.Security;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ChessClient.Models.Game;
+using ChessClient;
+using ChessClient.Controllers;
+using ChessClient.Models;
+using Common;
+
+
+namespace ChessClient.Tests.Model
+{
+    [TestClass]
+    public class ParseStateTest
+    {
+        [TestMethod]
+        public void TestStateParsing()
+        {
+            GameState state = new GameState();
+            GameModels m = new GameModels(state);
+            Figure f = new Figure();
+            f.Type = FigureType.Pawn;
+            f.Color = FigureColor.White;
+            Assert.AreEqual(m.cells[1, 1], f);
+        }
+    }
+}
--- ChessClient/Models/Game/GameModels.cs
+++ ChessClient/Models/Game/GameModels.cs
@@ -7,17 +7,18 @@ using Common;
 
 namespace ChessClient.Models
 {
-    public enum FigureColor { White, Black }
-    public enum FigureType { Pawn, Rook, Knight }
+    public enum FigureColor { White, Black, Empty }
+    public enum FigureType  { Pawn, Rook, Knight, Bishop, Queen, King, Empty }
     public struct Figure{
-        FigureColor Color;
-        FigureType Type;
+        public FigureColor Color;
+        public FigureType Type;
     }
 
     public class GameModels
     {
-        Figure[,] cells = new Figure[8, 8];
+        public Figure[,] cells = new Figure[8, 8];
         GameState gameState;
+        
         public GameModels(GameState gameState)
         {
             this.gameState = gameState;
@@ -26,20 +27,110 @@ namespace ChessClient.Models
 
         void Parse(string p)
         {
-            
+            const char EOLN='\n'; 
+            int  counter=0, rowNumber=0, value=0;
+            char cursor=p[0];
+            bool result=false;
+            while (cursor!=' ') {
+                result=System.Int32.TryParse(cursor.ToString(),out value);
+                if (result) {
+                    for (int i=1;i<=value;i++) {
+                        GetFigureType(cursor);
+                    }
+                    counter+=value;
+                    cursor=p[counter];
+                }
+                else { 
+                    cells[rowNumber,cursor]=GetFigureType(cursor);
+                    counter++;
+                    cursor=p[counter];
+                }
+            }
         }
-        public Figure GetCellInfo(int x, int y)
-        {
+
+        public Figure GetCellInfo(int x, int y) {
             return new Figure();
         }
-        public string GetFigureImage(int x, int y)
-        {
+
+        public string GetFigureImage(int x, int y) {
            
             return string.Empty;
         }
 
-        
-    
+        private Figure GetFigureType(char code) {
+            Figure temp=new Figure();
+            switch (code) {
+                case 'P': {
+                    temp.Color=FigureColor.Black;
+                    temp.Type=FigureType.Pawn;
+                    break;
+                }
+                case 'R' : {
+                       temp.Color=FigureColor.Black;
+                       temp.Type=FigureType.Rook;
+                       break;
+                }
+                case 'N' : {
+                       temp.Color=FigureColor.Black;
+                       temp.Type=FigureType.Knight;
+                       break;
+                }
+                case 'B' : {
+                       temp.Color=FigureColor.Black;
+                       temp.Type=FigureType.Bishop;
+                       break;
+                }
+                case 'K' : {
+                      temp.Color=FigureColor.Black;
+                      temp.Type=FigureType.King;
+                      break;
+                }
+                case 'Q' : {
+                      temp.Color=FigureColor.Black;
+                      temp.Type=FigureType.Queen;
+                      break;
+                }
+                case 'p': {
+                    temp.Color=FigureColor.White;
+                    temp.Type=FigureType.Pawn;
+                    break;
+                }
+                case 'r' : {
+                       temp.Color=FigureColor.White;
+                       temp.Type=FigureType.Rook;
+                       break;
+                }
+                case 'n' : {
+                       temp.Color=FigureColor.White;
+                       temp.Type=FigureType.Knight;
+                       break;
+                }
+                case 'b' : {
+                       temp.Color=FigureColor.White;
+                       temp.Type=FigureType.Bishop;
+                       break;
+                }
+                case 'k' : {
+                      temp.Color=FigureColor.White;
+                      temp.Type=FigureType.King;
+                      break;
+                }
+                case 'q' : {
+                      temp.Color=FigureColor.White;
+                      temp.Type=FigureType.Queen;
+                      break;
+                }
+                default:
+                    {
+                        temp.Color = FigureColor.Empty;
+                        temp.Type = FigureType.Empty;
+                        break;
+                    }
+            }
+            return temp;
+        }
+
+               
         public string[,] desk = new string[8, 8]
           {{"rookwhite.png","knightwhite.png","bishopwhite.png","queenwhite.png","kingwhite.png","bishopwhite.png","knightwhite.png","rookwhite.png"},
            {"pawnwhite.png","pawnwhite.png","pawnwhite.png","pawnwhite.png","pawnwhite.png","pawnwhite.png","pawnwhite.png","pawnwhite.png"},
@@ -50,38 +141,15 @@ namespace ChessClient.Models
            {"pawnblack.png","pawnblack.png","pawnblack.png","pawnblack.png","pawnblack.png","pawnblack.png","pawnblack.png","pawnblack.png"},
            {"rookblack.png","knightblack.png","bishopblack.png","queenblack.png","kingblack.png","bishopblack.png","knightblack.png","rookblack.png"}
           };
+
         public string move { get; set; }
 
 
         public string[] ConvertMove(string move){
              string[] str= move.Split(new Char[] {' '});
-             From = str[0];
-             To = str[1];
+             //From = str[0];
+             //To = str[1];
              return str;
-        }
-
-
-        public bool MakeMove(string move) {   
-            return true;
-        }
-
-        public void NewMove() { 
-            
-        }
-
-        public Cellij ConvertCell(string cell)
-        {
-            Cellij cellij = new Cellij();
-
-            return cellij;
-        
-        }
-        
-        private string From;
-        private string To;
-        public struct Cellij{
-            int i;
-            int j;
         }
 
     }
