8834988899ddd8dbfbab78965000cc2be2258b54
Iwillbeagod <ravenous944@gmail.com>
2013-11-15 22:42:44 +0400

    There is no changes, just fixed some bug

---
+++ ChessClient/Models/Game/ChessDesk.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ChessClient.Models.Game
+{
+    public class ChessDesk
+    {
+        public enum ChessColor { Black, White, Empty }
+        public struct CellNumber
+        {
+            public int col, row;
+        }
+    }
+}
