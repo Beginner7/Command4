7da6eec5e755c063e7c4b8f11a51b1dfe82a64a4
winchester7 <username@milov7.ru>
2013-11-18 22:12:58 +0400

    Adding 2 tests

---
+++ ChessService.Tests/Models/ChessDeskTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ChessService.Tests.Models
+{
+    [TestClass]
+    public class ChessDeskTest
+    {
+        [TestMethod]
+        public void CellToInt()
+        {
+            // Arrange
+            string cell = "F7";
+
+
+            // Act
+            int cellInt = ChessService.Models.ChessDesk.CellToInt(cell);
+
+            // Assert
+            Assert.AreEqual(cellInt, 46);
+        }
+
+        [TestMethod]
+        public void SetInitialStateDesk()
+        {
+            // Arrange
+
+
+            // Act
+            ChessService.Models.ChessDesk.Cell[] Desk = ChessService.Models.ChessDesk.SetInitialStateDesk();
+
+            // Assert
+            Assert.AreEqual(Desk[3].figureColor, true);
+            Assert.AreEqual(Desk[62].figureColor, false);
+            Assert.AreEqual(Desk[0].figureType, (int)ChessService.Models.ChessDesk.Figure.rook);
+        }
+    }
+}
