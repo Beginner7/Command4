c8b5f5a00796bad142816f33a856d85526a65ed7
winchester7 <username@milov7.ru>
2013-12-03 17:26:41 +0400

    Add Figure and Game

---
+++ ChessService/Models/Figure.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Common;
+
+namespace ChessService.Models {
+    enum GameState {
+
+    }
+    public abstract class Figure : ICloneable {
+        int x;
+        int y;
+        readonly FigureColor color;
+        public int[] Coordinate {
+            get { return new int[] { x, y }; }
+            set { x = value[0]; y = value[1]; }
+        }
+        public FigureColor Color { get { return color; } }
+
+        public Figure(int x, int y, FigureColor color) {
+            this.x = x;
+            this.y = y;
+            this.color = color;
+        }
+        public abstract object Clone();
+        public bool IsIt(int x, int y) {
+            return this.x == x && this.y == y;
+        }
+        public abstract List<int[]> GeneratePath(int x, int y);
+        public abstract bool CanEat(Figure another);
+        public abstract GameState CheckState(Figure another);
+    }
+
+    public class Pawn : Figure {
+        public Pawn(int x, int y, FigureColor color)
+            : base(x, y, color) {
+        }
+        public override object Clone() {
+            return new Pawn(Coordinate[0], Coordinate[1], Color);
+        }
+
+        public override List<int[]> GeneratePath(int x, int y) {
+            throw new NotImplementedException();
+        }
+
+        public override bool CanEat(Figure another) {
+            throw new NotImplementedException();
+        }
+
+        public override GameState CheckState(Figure another) {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ChessService/Models/Game.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ChessService.Models {
+    public class Game {
+        readonly List<Figure> figures;
+        public Game() {
+            figures = new List<Figure>();
+        }
+        public GameState ProcessMove(int xFrom, int yFrom, int xTo, int yTo) {
+            List<Figure> figuresCopy;
+        }
+    }
+}
