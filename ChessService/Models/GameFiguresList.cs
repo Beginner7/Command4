744b75a4f0042d9515f15788199a2ffc2f42815e
winchester7 <username@milov7.ru>
2013-12-10 17:35:51 +0400

    Add IsIt and MoveParse in GameFiguresList

--- ChessService/Models/GameFiguresList.cs
+++ ChessService/Models/GameFiguresList.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Text;
 using Common;
 
 namespace ChessService.Models {
@@ -15,8 +16,22 @@ namespace ChessService.Models {
             figures = new List<Figure>();
         }
         public CurrentGameResult ProcessMove(GameMove move) {
+            int[] coordinates = new int[4];
+            coordinates = move.MoveParse();
+
+            Figure currentFigure=null;
+            foreach(var figure in figures) {
+                if(figure.IsIt(coordinates[0], coordinates[1])) {
+                    currentFigure = figure;
+                    break;
+                }
+            }
+            if(currentFigure == null)
+                throw new ArgumentException();
+
             List<Figure> figuresCopy;
             return CurrentGameResult.Ok;
         }
+        
     }
 }
--- Common/GameMovie.cs
+++ Common/GameMovie.cs
@@ -10,5 +10,57 @@ namespace Common {
     public class GameMove {
         [DataMember]
         public string MoveNotation { get; set; }
+
+        public int[] MoveParse() {
+        string[] temp = MoveNotation.Split(new Char[] { ' ' });
+        string From = temp[0];
+        string To = temp[1];
+        
+        int FromX = 7 - (Convert.ToInt32(From[1]) - 49);
+        int FromY = ConvertSymboltoInt(From[0]);
+        int ToX = 7 - (Convert.ToInt32(To[1]) - 49);
+        int ToY = ConvertSymboltoInt(To[0]);
+
+        return new int[] { FromX, FromY, ToX, ToY };
+        }
+        public static int ConvertSymboltoInt(char symbol) {
+            int temp = 0;
+            switch(symbol) {
+                case 'a': {
+                        temp = 0;
+                        break;
+                    }
+                case 'b': {
+                        temp = 1;
+                        break;
+                    }
+                case 'c': {
+                        temp = 2;
+                        break;
+                    }
+                case 'd': {
+                        temp = 3;
+                        break;
+                    }
+                case 'e': {
+                        temp = 4;
+                        break;
+                    }
+                case 'f': {
+                        temp = 5;
+                        break;
+                    }
+                case 'g': {
+                        temp = 6;
+                        break;
+                    }
+                case 'h': {
+                        temp = 7;
+                        break;
+                    }
+
+            }
+            return temp;
+        }
     }
 }
