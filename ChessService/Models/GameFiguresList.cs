417ca1501ab88ab600190801fa118f121c64ff3e
winchester7 <username@milov7.ru>
2013-12-10 17:04:40 +0400

    Add GameFiguresList

--- ChessService/Models/GameFiguresList.cs
+++ ChessService/Models/GameFiguresList.cs
@@ -2,14 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Common;
 
 namespace ChessService.Models {
     public class GameFiguresList {
         readonly List<Figure> figures;
+        public GameState GameState { get; set; }
         public GameFiguresList() {
             figures = new List<Figure>();
         }
-        public CurrentGameResult ProcessMove(int xFrom, int yFrom, int xTo, int yTo) {
+        public GameFiguresList(GameState gameState) {
+            figures = new List<Figure>();
+        }
+        public CurrentGameResult ProcessMove(GameMove move) {
             List<Figure> figuresCopy;
             return CurrentGameResult.Ok;
         }
